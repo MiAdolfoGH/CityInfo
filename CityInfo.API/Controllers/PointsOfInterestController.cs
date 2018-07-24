4c13408c530be4c65f415e2988082163a5d562e4
Adolfo García <adolfo.garcia@entelgy.com>
2018-07-24 08:09:22 +0200

    Modulo 4.a

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Data;
+using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ namespace CityInfo.API.Controllers
 {
     // Hacemos que los PoI dependan de la ciudad, no pueden ser explotados de forma independiente.
     [Route("api/cities")]
-    public class PointsOfInterestController:Controller
+    public class PointsOfInterestController : Controller
     {
         [HttpGet("{cityId}/PointsOfInterest")]
         public IActionResult GetPointsOfInterest(int cityId)
@@ -24,7 +25,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/PointsOfInterest/{id}")]
+        [HttpGet("{cityId}/PointsOfInterest/{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -41,6 +42,82 @@ namespace CityInfo.API.Controllers
 
             return Ok(poi);
 
+        }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // Obtenemos el mayor indice del PointOfInterest
+            //TO-DO: Mejorar la forma de hacer este paso
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest"
+                , new { cityId = cityId, id = finalPointOfInterest.Id },
+                finalPointOfInterest);
+        }
+
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest( int cityId, int id,
+            [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description should be differente form the Name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poiFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poiFromStore == null)
+            {
+                return NotFound();
+            }
+
+            // PUT debe actualizar todos los campos
+            poiFromStore.Name = pointOfInterest.Name;
+            poiFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
         }
 
     }
